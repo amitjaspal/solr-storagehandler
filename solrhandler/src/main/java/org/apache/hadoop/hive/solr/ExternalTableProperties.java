6a5a45cc50636593b34618d9aa1dd1c01fd94aaa
Amit Jaspal <amit.jaspal@cloudera.com>
2014-07-20 23:04:49 -0700

    - Adding license header. - Adding some comments. - Adding Loggers.

--- solrhandler/src/main/java/org/apache/hadoop/hive/solr/ExternalTableProperties.java
+++ solrhandler/src/main/java/org/apache/hadoop/hive/solr/ExternalTableProperties.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
 import org.apache.hadoop.hive.ql.plan.TableDesc;
+
 
 /*
  * ExternalTableProperties is a holder to keep all the
@@ -20,23 +24,25 @@ public class ExternalTableProperties {
     static final String COLLECTION_NAME = "solr.collection.name";
     static final String SOLR_QUERY = "solr.query";
     static List<String> COLUMN_NAMES;
+    private Configuration conf;
+    static final Log LOG = LogFactory.getLog(SolrStorageHandler.class);
     
     public static void configureExternalTableProperties(Properties tableProperties, Map<String,String> jobProperties,
                                                         TableDesc tableDesc){
         
         // Set zookeeper.service.url in the jobProperty
         String zookeeperService = tableProperties.getProperty(ZOOKEEPER_SERVICE_URL);
-        System.out.println("Reading table property zookeeper url " + zookeeperService);
+        LOG.debug("Reading table property zookeeper url " + zookeeperService);
         jobProperties.put(ZOOKEEPER_SERVICE_URL, zookeeperService);
         
         // Set collection.name in the jobProperty
         String collectionName = tableProperties.getProperty(COLLECTION_NAME);
-        System.out.println("Reading table property collection name " + collectionName);
+        LOG.debug("Reading table property collection name " + collectionName);
         jobProperties.put(COLLECTION_NAME, collectionName);
         
         // Set SOLR query in the jobProperty
         String query = tableProperties.getProperty(SOLR_QUERY);
-        System.out.println("Reading table property solr query " + query);
+        LOG.debug("Reading table property solr query " + query);
         if(query == null){
             query = "*:*";
         }
--- solrhandler/src/main/java/org/apache/hadoop/hive/solr/SolrStorageHandler.java
+++ solrhandler/src/main/java/org/apache/hadoop/hive/solr/SolrStorageHandler.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.hive.solr;
 
 import java.util.ArrayList;
@@ -5,6 +23,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.HiveMetaHook;
 import org.apache.hadoop.hive.ql.index.IndexPredicateAnalyzer;
@@ -22,9 +42,12 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputFormat;
 
 
+
+
 public class SolrStorageHandler implements HiveStorageHandler, HiveStoragePredicateHandler{
     
     private Configuration conf;
+    static final Log LOG = LogFactory.getLog(SolrStorageHandler.class);
     
     @Override
     public Configuration getConf(){
